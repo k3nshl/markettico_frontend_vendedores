16c1c28f20a4416d152a3fa78dd4d3e43b0cf90e
YendryVO <149547539+YendryVO@users.noreply.github.com>
2023-11-15 10:27:35 -0600

    qqqquitar

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -201,7 +201,7 @@
                         </a>
                         <ul class="dropdown-menu dropdown-menu-end">
                             <li>
-                                <a class="dropdown-item" href="{{ route('perfil.show', ['id' => 1]) }}">
+                                <a class="dropdown-item" href="">
                                     <i class="bx bx-user"></i><span>Perfil</span>
                                 </a>
                             </li>
